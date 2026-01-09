0cce5f11545cbdbe22bc8c9cfc521d64fa2180d4
TKS2145 <t.sauntoo@alustudent.com>
2026-01-09 22:47:15 +0400

    Checked for git ignore and modified StudentViewer to allow different viewing options

--- StudentViewer.py
+++ StudentViewer.py
@@ -1,14 +1,26 @@
+from Globals import Studentlist
+
 def view_student(manager):
 
+    #Menu to be presented
+    view_all_Student()
+
+
+
+ 
+def view_all_Student():
+    for x in Studentlist:
+        print(x)
+
+def view_specific_student():
+
     student_id = input("Student ID: ")
-    student = manager.find_student(student_id)
-    
-    if student:
-        print(f"ID: {student.student_id}")
-        print(f"Name: {student.name}")
-        print(f"Class: {student.class_name}")
-        print(f"Total Fee: {student.total_fee}")
-        print(f"Paid: {student.amount_paid}")
-        print(f"Balance: {student.balance}")
-    else:
-        print(" Student not found")
+    for x in Studentlist:
+        if x.student_id == student_id :
+             print(f"ID: {x.student_id}")
+             print(f"Name: {x.name}")
+             print(f"Class: {x.class_name}")
+             print(f"Total Fee: {x.total_fee}")
+             print(f"Paid: {x.amount_paid}")
+             print(f"Balance: {x.balance}")
+
