dc4ccb2425e3d34c9dd44ffbaf7db1deb2ba798d
TKS2145 <t.sauntoo@alustudent.com>
2026-01-09 21:17:34 +0400

    Modified StudentAdder to allow adding a new student or directly adding an existing student to the list

--- StudentAdder.py
+++ StudentAdder.py
@@ -1,7 +1,30 @@
-def add_student(manager):
-    student_id = input("Student ID: ")
+import Globals
+from Classes import Student
+
+def new_student():
+
+    print("Enter details of student")
     name = input("Name: ")
     class_name = input("Class: ")
-    total_fee = float(input("Total Fee: "))
-    manager.add_student(student_id, name, class_name, total_fee)
+    while True:
+
+        try:
+            total_fee = float(input("Total Fee: "))
+            break
+        except:
+            print("Please only enter digits, decimal numbers accepted.")
+
+    add_student(name, class_name, total_fee) #Since toael fee should be fixed for classes, should I add a Class class for each class?
     print(" Student added successfully")
+
+def add_student(name, class_name, total_fee):
+
+    #increments studentcount to know the number of students available
+    Globals.StudentCount += 1
+    student_id = str(Globals.StudentCount) #Generates student ID from Student Count
+
+    #formats student_id into a 3 digit string
+    while len(student_id) < 3:
+        student_id = "0" + student_id
+
+    Globals.Studentlist.append(Student(student_id, name, class_name, total_fee,))
