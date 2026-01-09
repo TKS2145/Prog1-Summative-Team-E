42d29db2c4639c43e835c78e2f76cd36d4011e7d
TKS2145 <t.sauntoo@alustudent.com>
2026-01-10 00:45:23 +0400

    Modified PaymentRecorder to check before accepting a paymet. Fixed bugs also.

--- PaymentRecorder.py
+++ PaymentRecorder.py
@@ -1,13 +1,43 @@
 import Globals
+from Classes import Payment
 
 def record_payment():
 
     if Globals.StudentCount == 0: 
-       print("There are no students to reocrd any payments. Please register at least one student.")
+        print("There are no students to reocrd any payments. Please register at least one student.")
+        return
     
     else:
 
-        student_id = input("Student ID: ")
-        amount = float(input("Payment Amount: "))
+        while True: #Loop used to check if student id exist or not
+    
+            breaking = False #Used to exit loop is student id exist
+            student_id = input("Student ID: ")
+            for x in Globals.Studentlist:
+                if x.student_id == student_id :
+                    breaking = True
+                    break
+            
+            if breaking :
+                break
+            else:
+                print("Student ID does not exist. Press 1 to continue and 0 to exist.")
+                while True:
+                    choice = input("Enter 1 to continue or any other key to cancel.")
+                    if choice ==  1 :
+                        break
+                    else:
+                        return
+                    
+
+            while True:
+                try:
+                    amount = float(input("Payment Amount: "))
+                    break
+                except:
+                    print("Please enter payment account in digets (Decimal accepted")
+
+            new_payment = Payment(student_id, amount)
 
         print("Payment recorded successfully")
+        return
